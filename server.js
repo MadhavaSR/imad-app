0bb781cb5697cdef003589d50a3a082921c401c4
MadhavaSR <madhava.sr123@gmail.com>
2017-08-04 14:49:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,21 +5,87 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    articleone:{
+        title:'articelone',
+        date:'1 aug 2017',
+        content:` <p>
+                this is the content of my page article this is the content of my page article this is the content of my page article this is the content of my page article this is the content of my page article this is the content of my page article
+            </p>`,
+         heading:'Article-one'    
+    },
+    articletwo:{
+         title:'articelone',
+        date:'2 aug 2017',
+        content:` <p>
+                this is the content of my page article this is the content of my page article this is the content of my page article this is the content of my page article this is the content of my page article this is the content of my page article
+            </p>
+            <p>
+                this is the content of my page article this is the content of my page article this is the content of my page article this is the content of my page article this is the content of my page article this is the content of my page article
+            </p>`,
+        heading:'Article-one' 
+    },
+    articlethree:{
+         title:'articelthree',
+        date:'3 aug 2017',
+        content:` <p>
+                this is the content of my page article this is the content of my page article this is the content of my page article this is the content of my page article this is the content of my page article this is the content of my page article
+            </p>
+            <p>
+                this is the content of my page article this is the content of my page article this is the content of my page article this is the content of my page article this is the content of my page article this is the content of my page article
+            </p>
+            <p>
+                this is the content of my page article this is the content of my page article this is the content of my page article this is the content of my page article this is the content of my page article this is the content of my page article
+            </p>`,
+        heading:'Article-three' 
+    }
+    
+};
+
+
+
+
+function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var contnet=data.content;
+    var heading=data.heading;
+    
+    var htmlTemplate=`<html>
+    <head>
+        <title>${title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </head>
+    <body>
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h2>
+            ${heading}
+        </h2>
+        <div>
+            ${date}
+        </div>
+        <div>
+           ${content}
+        </div>
+    </body>
+</html>
+`
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req, res){
-	res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articleName', function(req, res){
+	var articleName = req.params.articleName;
+	res.send(createTemplate(articleName));
 });
 
-app.get('/article-two', function(req, res){
-	res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
 
-app.get('/article-three', function(req, res){
-	res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
